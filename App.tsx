9fee67e3cff10d357a333b0cefe772bacec7e0a5
Rosiele David <rosieleserpa@hotmail.com>
2023-01-17 13:42:41 -0300

    add fontes personalizadas e garante seu carregamento

--- App.tsx
+++ App.tsx
@@ -1,7 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter';
+
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if(!fontsLoaded) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
@@ -19,5 +41,6 @@ const styles = StyleSheet.create({
   },
   text: {
     color: 'white',
+    fontFamily: 'Inter_800ExtraBold',
   },
 });
